dfc71fa9398df9031d49b0242917323649ab9415
开放星河 <152999024+Open-Galaxy@users.noreply.github.com>
2023-12-13 22:15:56 +0800

    Add files via upload

---
+++ IEStart.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	string vbs_code = "CreateObject(\"InternetExplorer.Application\").Visible=True";
+	ofstream of("temp.vbs");
+	of << vbs_code;
+	of.close();
+
+	system("cscript temp.vbs");
+
+	return 0;
+}
